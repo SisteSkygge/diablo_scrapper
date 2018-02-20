15eab3aca119ffc415a5b010efd4f6b93fe7a986
SisteSkygge <lordigna@gmail.com>
2018-02-20 11:56:37 +0100

    Update test_bd.py

--- test_bd.py
+++ test_bd.py
@@ -5,12 +5,12 @@ def test_bd(db_var):
     requete = db_var.execute("SELECT * FROM Objet")
     for res in requete:
         print(res)
-    #requete = db_var.execute("SELECT * FROM Propriete")
-    #for res in requete:
-    #    print(res)
-    #requete = db_var.execute("SELECT * FROM Fabrique")
-    #for res in requete:
-    #    print(res)
+    requete = db_var.execute("SELECT * FROM Propriete")
+    for res in requete:
+        print(res)
+    requete = db_var.execute("SELECT * FROM Fabrique")
+    for res in requete:
+        print(res)
 
 if __name__=="__main__":
     db = sqlite3.connect(DB_NAME)
